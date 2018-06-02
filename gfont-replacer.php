a97faf31179b99d44518a5a9b0fe06883fa72a54
KevinTaron <kevin.taron@gmail.com>
2018-06-02 22:03:39 +0200

    Class Replace

--- gfont-replacer.php
+++ gfont-replacer.php
@@ -2,18 +2,20 @@
 /*
  Plugin Name: gFont Replace
  Plugin URI: https://gutwerker.de/
- Description: Replace Google Fonts with local Font. 
- Version: 0.1
+ Description: Replace all Google Fonts on your website with local fonts. Plugin downloads and serve the fonts from your server.  
+ Version: 0.2
  Author: Kevin Taron
  Author URI: https://gutwerker.de
- Text Domain: gfont-replacere
+ Text Domain: gw-gfont-replacer
+ License: GPLv3 or later
+ License URI: http://www.gnu.org/licenses/gpl-3.0.html
  */
 
 /*  Copyright 2018 Kevin Taron (email : k.taron@gutwerker.de)
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
+the Free Software Foundation; either version 3 of the License, or
 (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
@@ -26,110 +28,111 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+// No direct file access
+! defined( 'ABSPATH' ) AND exit;
+
 // Version of the plugin
-define('GW_GFONT_REPLACER_CURRENT_VERSION', '0.1' );
+define('GW_GFONT_REPLACER_CURRENT_VERSION', '0.2' );
 
 
-function gw_gfont_replacer_start_wp_head_buffer() {
-    ob_start();
-}
-add_action('wp_head','gw_gfont_replacer_start_wp_head_buffer',0);
+if ( ! class_exists( 'gw_gefont_replacer' ) ) {
 
-function gw_gfont_replacer_end_wp_head_buffer() {
-    $in = ob_get_clean();
+	add_action( 'init', array( 'gw_gefont_replacer', 'instance' ) );
 
-    $search = "<link.*href=.*(fonts.googleapis.com/css\?family=.*)>";
-    $mymatch = preg_match_all($search, $in, $output_array);
+	class gw_gefont_replacer {
 
-    if($mymatch) {
-	    $findlink = "/href=\"([^\"]*)\"/";
-	    $linkurl = preg_match_all($findlink, $output_array[0][0], $linkarray);
+		protected static $instance = null;
+		
+		//Create new instance in order to call outsite class.
+		public static function instance() {
+			null === self :: $instance AND self :: $instance = new self;
+			return self :: $instance;
+		}
 
-
-	    if(!$linkurl) {
-	    	$findlink = "/href='([^']*)'/";
-	    	$linkurl = preg_match_all($findlink, $output_array[0][0], $linkarray);
-	    }
-
-	    if($linkurl) {
-	    	$gfontlink = $linkarray[1][0];
-			gw_gfont_replacer_download_css($gfontlink);
-   			$in = gw_gfont_replacer_replace_headcsslinks($in, $gfontlink);
-	    }
-    	
-    }
+		public function __construct() {
+			add_filter( 'style_loader_src', array( $this,'gfont_replacer_load_cssfiles'), 100000, 2 );	
+		}
 
 
-    echo $in;
-}
-add_action('wp_head','gw_gfont_replacer_end_wp_head_buffer', PHP_INT_MAX);
+		function gfont_replacer_load_cssfiles( $src, $handle ) {
+			if ( ! preg_match("/fonts.googleapis.com/", $src, $output_array)) {
+				return $src;			
+			}
+
+			$localurl = $this->download_css($src);
+			return $localurl;
+		}
+
+		function checkIfGoogleFontsInCss($src) {
+			$cssfile = file_get_contents($src);
+			if(preg_match("/fonts.googleapis.com/", $cssfile, $output)) {
+				return true;
+			} 
+			return false;
+		}
+
+		function download_css($url) {
+			$cssfilename = $this->getCSSfileName($url);
+			$localurl = plugins_url( '/gfonts/css/' . $cssfilename, __FILE__ );
+			$localfile = plugin_dir_path( __FILE__ ) . "gfonts/css/" . $cssfilename; 
+			$file_exists = file_exists($localfile);
+
+			// if(!$file_exists) {
+			if(true) {
+				$url = 'https:' . $url;
+				$url = str_replace('https:https://', 'https://', $url);
+				$url = str_replace('https:http://', 'https://', $url);
+				$cssfile = file_get_contents($url);
+
+				$replacefile = $this->download_allfonts($cssfile);
+
+				$fp = fopen($localfile, 'w');
+				fwrite($fp, $replacefile);
+				fclose($fp);
+			}
+
+			return $localurl;
+		}
+
+		function download_allfonts($file) {
+			preg_match_all("/https:\/\/fonts.gstatic.com\/s\/[^)]*/", $file, $gfontfiles);
+			foreach ($gfontfiles[0] as $link) {
+				$this->download_single_font($link);
+			}
+			$file = $this->cssReplaceUrl($file);
+			return $file;
+		}
+
+		function cssReplaceUrl($file) {
+			$pluginurl = plugins_url( '/gfonts/fonts', __FILE__ );
+			$file =  preg_replace("/(https:\/\/fonts.gstatic.com\/s)/", $pluginurl ,$file);
+			return $file;
+		}
+
+		function download_single_font($gfonturl) {
+			$fontfile = file_get_contents($gfonturl);
+			$localfile = preg_replace("/https:\/\/fonts.gstatic.com\/s\//", plugin_dir_path( __FILE__ ) . "gfonts/fonts/", $gfonturl);
+			$localfilename = preg_match("/[^\/]*$/", $localfile, $localfilenamearray);
+			$localfilname = $localfilenamearray[0];
+			$localdir = str_replace($localfilname, '', $localfile);
 
 
-function gw_gfont_replacer_replace_headcsslinks($in, $linkurl) {
-	$newfilename = gw_gfont_replacer_getCSSfileName($linkurl);
-	$filelink = preg_match("/(family=)(.*)/", $linkurl, $output_array);
-	$in = str_replace( '//fonts.googleapis.com/css?family=', plugins_url( '/gfonts/css/', __FILE__ ), $in );
-	$in = str_replace( $output_array[2], $newfilename, $in );
-	$in = str_replace( '//fonts.googleapis.com', network_site_url( '/' ), $in );
+			if (!is_dir($localdir)) {
+			    mkdir($localdir, 0777, true); // true for recursive create
+			}
 
-	return $in;
-}
-
+			$fp = fopen($localfile, 'w');
+			fwrite($fp, $fontfile);
+			fclose($fp);
+		}
 
 
-
-function gw_gfont_replacer_download_css($url) {
-	$cssfilename = gw_gfont_replacer_getCSSfileName($url);
-	$localurl = plugins_url( '/gfonts/css/' . $cssfilename, __FILE__ );
-	$localfile = plugin_dir_path( __FILE__ ) . "gfonts/css/" . $cssfilename; 
-	$file_exists = file_exists($localfile);
-
-	if(!$file_exists) {
-		$cssfile = file_get_contents('https:' . $url);
-		$replacefile = gw_gfont_replacer_download_gfonts($cssfile);
-
-		$fp = fopen($localfile, 'w');
-		fwrite($fp, $replacefile);
-		fclose($fp);
-	}
-
-	return $localurl;
-}
+		function getCSSfileName($url) {
+			$filelink = preg_match("/(family=)(.*)/", $url, $output_array);
+			return md5($output_array[2]) . '.css';
+		}
 
 
-function gw_gfont_replacer_getCSSfileName($url) {
-	$filelink = preg_match("/(family=)(.*)/", $url, $output_array);
-	return md5($output_array[2]) . '.css';
-}
+	} // END
 
-function gw_gfont_replacer_download_gfonts($file) {
-	preg_match_all("/https:\/\/fonts.gstatic.com\/s\/[^)]*/", $file, $gfontfiles);
-	foreach ($gfontfiles[0] as $link) {
-		gw_gfont_replacer_download_single_gfont($link);
-	}
-	$file = gw_gfont_replacer_cssReplaceUrl($file);
-	return $file;
-}
-
-function gw_gfont_replacer_cssReplaceUrl($file) {
-	$pluginurl = plugins_url( '/gfonts/fonts', __FILE__ );
-	$file =  preg_replace("/(https:\/\/fonts.gstatic.com\/s)/", $pluginurl ,$file);
-	return $file;
-}
-
-function gw_gfont_replacer_download_single_gfont($gfonturl) {
-	$fontfile = file_get_contents($gfonturl);
-	$localfile = preg_replace("/https:\/\/fonts.gstatic.com\/s\//", plugin_dir_path( __FILE__ ) . "gfonts/fonts/", $gfonturl);
-	$localfilename = preg_match("/[^\/]*$/", $localfile, $localfilenamearray);
-	$localfilname = $localfilenamearray[0];
-	$localdir = str_replace($localfilname, '', $localfile);
-
-
-	if (!is_dir($localdir)) {
-	    mkdir($localdir, 0777, true); // true for recursive create
-	}
-
-	$fp = fopen($localfile, 'w');
-	fwrite($fp, $fontfile);
-	fclose($fp);
-}
+} // endif;
